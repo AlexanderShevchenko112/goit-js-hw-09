c57473fe636acc70c7ebce790f96fb862cd7a9b1
AlexanderShevchenko112 <aleks.shevchenko.2013@gmail.com>
2023-02-05 21:24:51 +0200

    completed task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-const startBtn = document.querySelector(button);
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector(`button[data-stop]`);
+const body = document.querySelector('body');
+let timerId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener(`click`, changeColor);
+stopBtn.addEventListener(`click`, freezeColor);
+
+function changeColor() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function freezeColor() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+}
